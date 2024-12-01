bfd7516e7213be4fd92aa218ad204433d22e4f7c
kicm <123@qq.com>
2024-12-01 21:26:37 +0800

    test

--- templates/admin/left.php
+++ templates/admin/left.php
@@ -5,7 +5,7 @@
         <li class="layui-nav-item layui-nav-itemed">
           <a class="" href="javascript:;">分类管理</a>
           <dl class="layui-nav-child">
-            <dd><a href="/index.php?c=admin&page=category_list">分类列表</a></dd>
+            <dd><a href="/index.php?c=admin&page=category_list">分类列表1</a></dd>
             <dd><a href="/index.php?c=admin&page=add_category">添加分类</a></dd>
           </dl>
         </li>
